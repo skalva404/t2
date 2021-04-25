78ceb2e7ae51ee8b804e271174d644d3ffcd0de0
sunilkumar kalva <sunilkumar.kalva@swiggy.in>
2021-04-25 15:11:49 +0530

    inital commit

--- src/main/java/com/fetchData/t2/UserModel.java
+++ src/main/java/com/fetchData/t2/UserModel.java
@@ -1,7 +1,12 @@
 package com.fetchData.t2;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "permission")
 public class UserModel {
@@ -31,70 +36,5 @@ public class UserModel {
 
     @Column(name = "lastUpdateDate")
     String lastUpdateDate;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String nAME) {
-        name = nAME;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String dESCRIPTION) {
-        description = dESCRIPTION;
-    }
-
-    public String getFeature() {
-        return feature;
-    }
-
-    public void setFeature(String fEATURE) {
-        feature = fEATURE;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String tYPE) {
-        type = tYPE;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String sTATUS) {
-        status = sTATUS;
-    }
-
-    public String getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(String cREATE_DATE) {
-        createDate = cREATE_DATE;
-    }
-
-    public String getLastUpdateDate() {
-        return lastUpdateDate;
-    }
-
-    public void setLastUpdateDate(String lAST_UPDATE_DATE) {
-        lastUpdateDate = lAST_UPDATE_DATE;
-    }
-
 
 }
