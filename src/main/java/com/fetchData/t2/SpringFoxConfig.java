3134e40496be472d303ff8bfe12a06a6a9b80fe8
sunilkumar kalva <sunilkumar.kalva@swiggy.in>
2021-04-25 15:28:55 +0530

    inital commit

---
+++ src/main/java/com/fetchData/t2/SpringFoxConfig.java
@@ -0,0 +1,17 @@
+package com.fetchData.t2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.fetchData.t2"))
+                .build();
+    }
+}
--- src/main/java/com/fetchData/t2/T2Application.java
+++ src/main/java/com/fetchData/t2/T2Application.java
@@ -2,7 +2,9 @@ package com.fetchData.t2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class T2Application {
 
